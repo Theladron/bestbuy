6f1619c53e26561e2f52d6fd143cd2c662f30c25
Theladron <Philwolframm@gmail.com>
2025-03-03 02:09:14 +0100

    Deleted rest of testing code from store.py

--- store.py
+++ store.py
@@ -17,28 +17,22 @@ class Store:
 
 
     def get_total_quantity(self):
-        self.total_products = 0
+        total_products = 0
         for product in self.products:
-            self.total_products += product.get_quantity()
-        return f"Total of {self.total_products} items in store"
+            total_products += product.get_quantity()
+        return f"Total of {total_products} items in store"
 
 
     def get_all_products(self):
-        self.active_products = []
+        active_products = []
         for product in self.products:
             if product.is_active():
-                self.active_products.append(product)
-        return self.active_products
+                active_products.append(product)
+        return active_products
 
 
     def order(self, shopping_list):
-        self.total_price = 0
+        total_price = 0
         for product, quantity in shopping_list:
-            self.total_price += product.buy(quantity)
-        return f"Total order price: {self.total_price}"
-
-
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
-               ]
+            total_price += product.buy(quantity)
+        return f"Total order price: {total_price}"
